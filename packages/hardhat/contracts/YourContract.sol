d356086a352247cd4d4176425072e7c02c209b28
munair <munair@gmail.com>
2021-07-28 15:33:13 -0400

    adding payable

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -9,7 +9,7 @@ contract YourContract {
   bool public payer_approved; //the person giving funds gives consent to transfer funds out of the smart contracts.
   bool public payee_approved; //the person receiving funds gives consent to transfer funds out of the smart contracts.
 
-  constructor(address payable _payee) {
+  constructor(address payable _payee) payable {
     //this is done on deploy
     payer = payable(msg.sender);
     payee = _payee;
