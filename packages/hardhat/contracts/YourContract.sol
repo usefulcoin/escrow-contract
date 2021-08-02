2ea11ab6d8c97d6b1c177288fbd36e5eb884839b
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-08-02 13:11:12 -0400

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -8,11 +8,16 @@ contract YourContract {
   address payable public payee; //this is the person receiving funds.
   bool public payer_approved; //the person giving funds gives consent to transfer funds out of the smart contracts.
   bool public payee_approved; //the person receiving funds gives consent to transfer funds out of the smart contracts.
+  bool public payee_set;
 
-  constructor(address payable _payee) payable {
-    //this is done on deploy
+  constructor() payable {
     payer = payable(msg.sender);
-    payee = _payee;
+  }
+  
+  function setPayee(address payable _payee) public onlyPayer {
+      require(!payee_set, "payee is already set");
+      payee = _payee;
+      payee_set = true;
   }
 
   modifier onlyPayer() {
@@ -54,7 +59,7 @@ contract YourContract {
     require(payer_approved, "payer has not approved.");
     payee.transfer(address(this).balance);
   }
-
+  
   fallback() external payable {}
 
 }
